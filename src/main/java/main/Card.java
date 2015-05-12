6299648f2d46f677e7f6cf04529a3ecf245feb35
dayFun <joethorngren@gmail.com>
2015-05-11 23:09:20 -0500

    Wrapped tests around PD card class.  Not testing logging, going to pull that out into a different class. setInterestCharge() is full of bugs. About to refactor this bitch.

--- src/main/java/main/Card.java
+++ src/main/java/main/Card.java
@@ -6,21 +6,17 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.DateFormat;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.text.NumberFormat;
 
-/**
- *
- * @author
- */
 public class Card {
     private double CLimit, BalDue;
     private int AcctNo;
     private String actmsg, errmsg;
     NumberFormat c = NumberFormat.getCurrencyInstance();
     NumberFormat p = NumberFormat.getPercentInstance();
-    
+
     public Card() {
         //constructor for a 'create new' operation
         this.AcctNo = 0;
@@ -28,12 +24,11 @@ public class Card {
         this.BalDue = 0;
         this.actmsg = "";
         this.errmsg = "";
-        
+
         while (this.AcctNo == 0) {
             try {
                 this.AcctNo = (int) (Math.random() * 1000000);
-                BufferedReader in = new BufferedReader(
-                                     new FileReader("CC" + this.AcctNo + ".txt"));
+                BufferedReader in = new BufferedReader(new FileReader("CC" + this.AcctNo + ".txt"));
                 in.close();
                 this.AcctNo = 0;
             } catch (IOException e) {
@@ -41,9 +36,9 @@ public class Card {
                 this.CLimit = 1000;
                 writestatus();
                 if (this.errmsg.isEmpty()) {
-                   actmsg = "Account " + this.AcctNo + " opened.";
-                   writelog(actmsg);
-                }   
+                    actmsg = "Account " + this.AcctNo + " opened.";
+                    writelog(actmsg);
+                }
                 if (!this.errmsg.isEmpty()) {
                     this.CLimit = 0;
                     this.AcctNo = -1;
@@ -54,17 +49,16 @@ public class Card {
             }
         }
     } //end of constructor
-    
+
     public Card(int a) {
         errmsg = "";
         actmsg = "";
         this.CLimit = 0;
         this.BalDue = 0;
         this.AcctNo = a;
-        
+
         try {
-            BufferedReader in = new BufferedReader(
-                                new FileReader("CC" + this.AcctNo + ".txt"));
+            BufferedReader in = new BufferedReader(new FileReader("CC" + this.AcctNo + ".txt"));
             this.CLimit = Double.parseDouble(in.readLine());
             this.BalDue = Double.parseDouble(in.readLine());
             in.close();
@@ -74,28 +68,26 @@ public class Card {
             this.AcctNo = -1;
         }
     }
-    
+
     private void writestatus() {
         try {
-            PrintWriter out = new PrintWriter(
-                              new FileWriter("CC" + this.AcctNo + ".txt"));
+            PrintWriter out = new PrintWriter(new FileWriter("CC" + this.AcctNo + ".txt"));
             out.println(this.CLimit);
             out.println(this.BalDue);
             out.close();
         } catch (IOException e) {
             errmsg = "Error writing status file for account: " + this.AcctNo;
-        } catch(Exception e) {
+        } catch (Exception e) {
             errmsg = "General error in status update: " + e.getMessage();
         }
     } //end of writestatus
-    
+
     private void writelog(String msg) {
         try {
             Calendar cal = Calendar.getInstance();
             DateFormat df = DateFormat.getDateTimeInstance();
             String ts = df.format(cal.getTime());
-            PrintWriter out = new PrintWriter(
-                              new FileWriter("CCL" + this.AcctNo + ".txt",true));
+            PrintWriter out = new PrintWriter(new FileWriter("CCL" + this.AcctNo + ".txt", true));
             out.println(msg + "\t" + ts);
             out.close();
         } catch (IOException e) {
@@ -104,71 +96,68 @@ public class Card {
             errmsg = "General error in write log: " + e.getMessage();
         }
     } //end of writelog
-    
+
     public int getAccountNo() {
         return this.AcctNo;
     }
-    
+
     public double getCreditLimit() {
         return this.CLimit;
     }
-    
+
     public double getBalanceDue() {
         return this.BalDue;
     }
-    
+
     public double getAvailCredit() {
         return (this.CLimit - this.BalDue);
     }
-    
+
     public String getErrorMsg() {
         return this.errmsg;
     }
-    
+
     public String getActionMsg() {
         return this.actmsg;
     }
-    
+
     public void setCharge(double amt, String desc) {
         errmsg = "";
         actmsg = "";
-        
+
         if (this.AcctNo <= 0) {
             errmsg = "Charge attempt on non-active account.";
             return;
         }
-        
+
         if (amt <= 0) {
-           actmsg = "Charge of " + c.format(amt) + " for " + desc +
-                    " declined - illegal amount.";
-           writelog(actmsg);
-        } else if((this.BalDue + amt) > this.CLimit) {
-           actmsg = "Charge of " + c.format(amt) + " for " + desc +
-                    " declined - over limit!"; 
-           writelog(actmsg);
+            actmsg = "Charge of " + c.format(amt) + " for " + desc + " declined - illegal amount.";
+            writelog(actmsg);
+        } else if ((this.BalDue + amt) > this.CLimit) {
+            actmsg = "Charge of " + c.format(amt) + " for " + desc + " declined - over limit!";
+            writelog(actmsg);
         } else {
-           this.BalDue += amt;
-           writestatus();
-           if (this.errmsg.isEmpty()) {
-               actmsg = "Charge of " + c.format(amt) + " for " + desc +
-                        " posted.";
-               writelog(actmsg);
-           }
+            this.BalDue += amt;
+            writestatus();
+            if (this.errmsg.isEmpty()) {
+                actmsg = "Charge of " + c.format(amt) + " for " + desc + " posted.";
+                writelog(actmsg);
+            }
         }
     } //end of setcharge
-    
+
     public void setPayment(double amt) {
         errmsg = "";
         actmsg = "";
-        
+
         if (this.AcctNo <= 0) {
             errmsg = "Charge attempt on non-active account.";
             return;
         }
-        
+
         if (amt <= 0) {
             actmsg = "Payment of " + c.format(amt) + " declined - illegal amount.";
-            writelog(actmsg); 
+            writelog(actmsg);
         } else {
             this.BalDue -= amt;
             writestatus();
@@ -177,54 +166,53 @@ public class Card {
                 writelog(actmsg);
             }
         }
-   } //end of payment
-    
-   public ArrayList<String> getLog() {
-       ArrayList<String> h = new ArrayList<String>();
-       errmsg = "";
-       actmsg = "";
-       String t;
-       
-       if (this.AcctNo <= 0) {
+    } //end of payment
+
+    public ArrayList<String> getLog() {
+        ArrayList<String> h = new ArrayList<String>();
+        errmsg = "";
+        actmsg = "";
+        String t;
+
+        if (this.AcctNo <= 0) {
             errmsg = "Charge attempt on non-active account.";
             return h;
         }
-       
-       try {
-           BufferedReader in = new BufferedReader(
-                               new FileReader("CCL" + this.AcctNo + ".txt"));
-           t = in.readLine();
-           
-           while (t != null) {
-              h.add(t);
-              t = in.readLine();
-           }
-           in.close();
-           actmsg = "History returned for account: " + this.AcctNo;
-       } catch (Exception e) {
-           errmsg = "Error reading log file: " + e.getMessage();
-       }
-       return h;
-   }
-   
-   public void setInterestCharge(double ir) {
-       errmsg = "";
-       actmsg = "";
-       double intchg;
-       
-       if (this.AcctNo <= 0) {
-           errmsg = "Interest Charge attempt on non-active account.";
-           return;
-       }
-       
-       if (ir <= 0) {
-           actmsg = "Interest rate of " + p.format(ir) + " declined - illegal amount.";
-           writelog(actmsg); 
-       } else {
-           intchg = this.BalDue * ir/12.00;
-           setCharge(intchg,"Interest charged.");
-           //The writestatus() and writelog() methods will be performed in the setCharge() method
-           
-       } //end of interest charge method
-   }
+
+        try {
+            BufferedReader in = new BufferedReader(new FileReader("CCL" + this.AcctNo + ".txt"));
+            t = in.readLine();
+
+            while (t != null) {
+                h.add(t);
+                t = in.readLine();
+            }
+            in.close();
+            actmsg = "History returned for account: " + this.AcctNo;
+        } catch (Exception e) {
+            errmsg = "Error reading log file: " + e.getMessage();
+        }
+        return h;
+    }
+
+    public void setInterestCharge(double ir) {
+        errmsg = "";
+        actmsg = "";
+        double intchg;
+
+        if (this.AcctNo <= 0) {
+            errmsg = "Interest Charge attempt on non-active account.";
+            return;
+        }
+
+        if (ir <= 0) {
+            actmsg = "Interest rate of " + p.format(ir) + " declined - illegal amount.";
+            writelog(actmsg);
+        } else {
+            intchg = this.BalDue * ir / 12.00;
+            setCharge(intchg, "Interest charged.");
+            //The writestatus() and writelog() methods will be performed in the setCharge() method
+
+        } //end of interest charge method
+    }
 }
--- src/test/java/main/CardTest.java
+++ src/test/java/main/CardTest.java
@@ -1,6 +1,155 @@
 package main;
 
+import static org.junit.Assert.assertEquals;
+
+import java.text.NumberFormat;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
 
 public class CardTest {
 
+    private static final double NEW_ACCOUNT_STARTING_BALANCE = 0;
+    private static final double NEW_ACCOUNT_CREDIT_LIMIT = 1000;
+    private NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance();
+    private NumberFormat percentFormatter = NumberFormat.getPercentInstance();
+
+    private Card testCard;
+
+    @Before
+    public void setup() {
+        testCard = new Card();
+    }
+
+    @Test
+    public void testWhenNewAccountCreatedThenBalanceIsZero() {
+        assertEquals(NEW_ACCOUNT_STARTING_BALANCE, testCard.getBalanceDue(), 0);
+    }
+
+    @Test
+    public void testWhenNewAccountCreatedThenCreditLimitIs1000() {
+        assertEquals(NEW_ACCOUNT_CREDIT_LIMIT, testCard.getCreditLimit(), 0);
+    }
+
+    @Test
+    public void testCreateCardWithExistingAccountReopensAccount() {
+        testCard.setCharge(100.0, "Electric Bill");
+        double expectedBalance = testCard.getBalanceDue();
+        double expectedCredit = testCard.getAvailCredit();
+        int existingAccountNumber = testCard.getAccountNo();
+        String expectedMessage = "Account " + existingAccountNumber + " re-opened.";
+
+        Card newCard = new Card(existingAccountNumber);
+
+        assertEquals(expectedMessage, newCard.getActionMsg());
+        assertEquals(expectedBalance, newCard.getBalanceDue(), 0);
+        assertEquals(expectedCredit, newCard.getAvailCredit(), 0);
+    }
+
+    @Test
+    public void testValidChargeIncreasesBalanceAndDecreasesCreditLimit() {
+        double chargeAmount = 50.00;
+        String description = "Porn";
+        testCard.setCharge(chargeAmount, description);
+
+        double expectedBalance = chargeAmount - NEW_ACCOUNT_STARTING_BALANCE;
+        double expectedCreditLimit = NEW_ACCOUNT_CREDIT_LIMIT - chargeAmount;
+        String expectedMessage = createChargeLogMessageTemplate(chargeAmount, description) + " posted.";
+
+        assertEquals(expectedBalance, testCard.getBalanceDue(), 0);
+        assertEquals(expectedCreditLimit, testCard.getAvailCredit(), 0);
+        assertEquals(expectedMessage, testCard.getActionMsg());
+    }
+
+    @Test
+    public void testChargeWithNegativeAmountThenTransactionDeclined() {
+        double expectedBalance = testCard.getBalanceDue();
+        double chargeAmount = -50.00;
+        String chargeDescription = "Booze";
+
+        testCard.setCharge(chargeAmount, chargeDescription);
+
+        String expectedErrorMessage = createChargeLogMessageTemplate(chargeAmount, chargeDescription) + " declined - illegal amount.";
+
+        //TODO: Currency formatter removes negative symbol?
+
+        assertEquals(expectedErrorMessage, testCard.getActionMsg());
+        assertEquals(expectedBalance, testCard.getBalanceDue(), 0);
+    }
+
+    @Test
+    public void testWhenChargeOverCreditLimitThenTransactionDeclined() {
+        double expectedBalance = testCard.getBalanceDue();
+        double chargeAmount = 10000;
+        String chargeDescription = "Hooker";
+
+        testCard.setCharge(chargeAmount, chargeDescription);
+
+        String expectedErrorMessage = createChargeLogMessageTemplate(chargeAmount, chargeDescription) + " declined - over limit!";
+
+        assertEquals(expectedErrorMessage, testCard.getActionMsg());
+        assertEquals(expectedBalance, testCard.getBalanceDue(), 0);
+    }
+
+    @Test
+    public void testMakePaymentWithNegativeAmountRejectsPayment() {
+        double expectedBalance = testCard.getBalanceDue();
+        double paymentAmount = -250.00;
+
+        testCard.setPayment(paymentAmount);
+
+        String expectedErrorMessage = createPaymentLogMessageTemplate(paymentAmount) + " declined - illegal amount.";
+
+        assertEquals(expectedErrorMessage, testCard.getActionMsg());
+        assertEquals(expectedBalance, testCard.getBalanceDue(), 0);
+    }
+
+    @Test
+    public void testMakeValidPaymentDecreasesBalanceAndIncreasesCreditLimit() {
+        double chargeAmount = 500.0;
+        double paymentAmount = 250.00;
+        double expectedBalance = chargeAmount - paymentAmount;
+        String expectedErrorMessage = createPaymentLogMessageTemplate(paymentAmount) + " posted.";
+        testCard.setCharge(chargeAmount, "Plane Ticket");
+
+        testCard.setPayment(paymentAmount);
+
+        assertEquals(expectedErrorMessage, testCard.getActionMsg());
+        assertEquals(expectedBalance, testCard.getBalanceDue(), 0);
+    }
+
+    @Test
+    public void testDoNotChargeNegativeInterest() {
+        double illegalInterestRate = -75.0;
+        double expectedBalance = testCard.getBalanceDue();
+        double expectedCreditLimit = testCard.getAvailCredit();
+        String expectedErrorMessage = "Interest rate of " + percentFormatter.format(illegalInterestRate) + " declined - illegal amount.";
+
+        testCard.setInterestCharge(illegalInterestRate);
+
+        assertEquals(expectedErrorMessage, testCard.getActionMsg());
+        assertEquals(expectedBalance, testCard.getBalanceDue(), 0);
+        assertEquals(expectedCreditLimit, testCard.getAvailCredit(), 0);
+    }
+
+    @Test
+    @Ignore
+    public void testWhenBalanceIs1000AndInterestRateIs10PercentThenInterestChargeIs100() {
+        testCard.setCharge(1000.0, "Rent");
+        testCard.setInterestCharge(10.0);
+
+        String incorrectErrorMessage = createChargeLogMessageTemplate(100.0, "Interest charged.") + " declined - illegal amount.";
+
+        assertEquals(incorrectErrorMessage, testCard.getActionMsg());
+    }
+
+    private String createChargeLogMessageTemplate(double amount, String description) {
+        return "Charge of " + currencyFormatter.format(amount) + " for " + description;
+    }
+
+    private String createPaymentLogMessageTemplate(double amount) {
+        return "Payment of " + currencyFormatter.format(amount);
+    }
 }
